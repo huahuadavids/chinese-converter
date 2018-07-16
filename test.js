c908d490f9735d897d129ba3228912f66d53ebbd
starbugs <zyh2@nuoee.com>
2018-07-16 11:47:20 +0800

    init

---
+++ test.js
@@ -0,0 +1,14 @@
+var S = require("./index");
+var c = require("./c")
+var fs = require("fs")
+c.forEach(function(item,index){
+
+  item[2] = S.s2t(item[2])
+  // console.log(item)
+  // console.log(',')
+})
+
+fs.writeFile('./country.json', JSON.stringify(c),function(err){
+  if(err) console.log('写文件操作失败');
+  else console.log('写文件操作成功');
+});
